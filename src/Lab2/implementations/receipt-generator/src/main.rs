7e7c0c17dcce19306b6090c9383f9d292ac28ea1
SoTrx <-->
2022-05-09 13:24:03 +0200

    Lab 2: Updating receipt-generator service to retrive its mailing url from its env. Also making it more verbose

--- src/Lab2/implementations/receipt-generator/src/main.rs
+++ src/Lab2/implementations/receipt-generator/src/main.rs
@@ -1,6 +1,6 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use std::{convert::Infallible, env, net::SocketAddr, collections::HashMap};
+use std::{collections::HashMap, convert::Infallible, env, net::SocketAddr};
 mod types;
 extern crate dotenv;
 use dotenv::dotenv;
@@ -9,9 +9,17 @@ use log::{error, info, warn};
 const SERVER_PORT: u16 = 8081;
 const SERVER_HOST: [u8; 4] = [127, 0, 0, 1];
 
-
-async fn mail_receipt()-> types::Result<bool> {
-    let mail_service_url: String = String::from("http://localhost:3500/v1.0/bindings/mailing");
+async fn mail_receipt() -> types::Result<bool> {
+    let mail_service_url;
+    match env::var("MAILING_BINDING_URL") {
+        Ok(val) => mail_service_url = val,
+        Err(_e) => mail_service_url = "none".to_string(),
+    }
+    if mail_service_url == "none" {
+        info!("No mail binding defined, wait for the binding part on the workshop");
+        return Ok(false)
+    }
+    //"http://localhost:3500/v1.0/bindings/mailing"
     let client = reqwest::Client::new();
 
     // Prepare mail body, using a map as a json substitute
@@ -20,10 +28,9 @@ async fn mail_receipt()-> types::Result<bool> {
     json.insert("to", "json");
 
     let res = client.post(mail_service_url).json(&json).send().await?;
-    if res.status().is_success(){
+    if res.status().is_success() {
         info!("Successfully sent mail")
-    }
-    else {
+    } else {
         warn!("Couldn't send mail")
     }
     Ok(true)
@@ -31,11 +38,11 @@ async fn mail_receipt()-> types::Result<bool> {
 
 /// Search and return a random cat image
 async fn generate_receipt(_req: Request<Body>) -> types::Result<Response<Body>> {
+    info!("Receipt generator woken up");
     mail_receipt().await?;
+    info!("Receipt generator succeded");
     Ok(Response::new("Ok".into()))
 }
-
-
 
 #[tokio::main]
 async fn main() {
